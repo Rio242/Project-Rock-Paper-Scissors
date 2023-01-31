3a35085f714d8780a85ae596ae0d426f7cb2390e
Mario Strachan <96462826+Rio242@users.noreply.github.com>
2023-01-31 03:24:17 +0000

    created randomizer function

---
+++ script.js
@@ -0,0 +1,15 @@
+// 1. make random number generator function between 1 and 3
+
+// 2.
+
+//3. 
+
+
+
+
+
+function getComputerChoice(selection){
+    return Math.ceil(Math.random()*selection)
+}
+
+console.log(getComputerChoice())
